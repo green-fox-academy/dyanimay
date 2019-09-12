e8cbfb8f0f05b0ad4bd16926980ccbd26d145aab
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 14:08:40 +0200

    csak az eredmenyek

---
+++ week-01/day-4/two-numbers.ts
@@ -0,0 +1,8 @@
+let myNum1: number = 22;
+let myNum2: number = 13;
+
+console.log(myNum1 + myNum2);
+console.log(myNum2 - myNum1);
+console.log(myNum1 * myNum2);
+console.log(myNum1 / myNum2);
+console.log(myNum1 % myNum2);

43b49424d21bc7cd8d51375cbb1943a52245a6ec
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 16:28:02 +0200

    BMI megvan

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let massInKg: number = 73.4;
+let heightInM: number = 1.83;
+
+// Print the Body mass index (BMI) based on these values
+console.log("My BMI is: " + (massInKg/(heightInM*heightInM)).toFixed(2) + ".");

b072f070ec3f444945794c831deed3f5124ae838
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 16:42:59 +0200

    csak sikerult a boolean

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myName: string = "Daniel Kristaly";
+// Your age as a number
+let myAge: number = 27;
+// Your height in meters as a number
+let myHeight: number = 1.83;
+// Whether you are married or not as a boolean
+let married: boolean = true;
+let notMarried: boolean = false;
+
+console.log("My name is " + myName + ". I am " + myAge + " years old. I am " + myHeight + "m tall.");
+if (married && notMarried) {
+    console.log("I am married.");
+  } else {
+    console.log("I am not married yet.");
+  }

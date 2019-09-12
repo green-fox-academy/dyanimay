59a2790dd41e1827207ec538b1ae3c4d14292e0a
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 13:43:42 +0200

    added 3 names to greet

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,9 @@
+'use strict'
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Reka!
+console.log("Good day, Reka!")
+// Hello, Matt!
+console.log("Hello, Matt! How are you?")
+// Hello, Andras!
+console.log("Greetings, Andras!")

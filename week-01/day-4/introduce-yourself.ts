b60739c30cac1890fa966c68d3e469b12a319746
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 13:59:45 +0200

    introduction is added

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,8 @@
+'use strict';
+let myName: string = "Daniel Kristaly";
+let myAge: number = 27;
+let myHeight: number = 1.83;
+
+console.log("My name is " + myName + ".");
+console.log("I am " + myAge + " years old.");
+console.log("I am " + myHeight + "m tall.");

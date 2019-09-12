b14d4f9c578d88e03e7d730a861de2f5c4a83f7f
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-12 16:22:23 +0200

    megcsereltem

---
+++ week-01/day-4/swap.ts
@@ -0,0 +1,9 @@
+'use strict';
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+console.log("a" + " is " + a);
+console.log("b" + " is " + b);
+console.log("AND NOW SWAP THEM");
+console.log("a" + " became " + b);
+console.log("b" + " became " + a);

dca358982bafb9757397e379c67e2d57c7c017c8
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-13 09:34:00 +0200

    i can not do supersript cm

---
+++ week-01/day-5/cuboid.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+let sideA: number = 10.5;
+let sideB: number = 15.4;
+let sideC: number = 22.2;
+// Surface Area: 600
+// Volume: 1000
+console.log("Surface Area: " + (sideA*sideB).toFixed(2));
+console.log("Volume of the cube: " + (sideA*sideB*sideC).toFixed(2));

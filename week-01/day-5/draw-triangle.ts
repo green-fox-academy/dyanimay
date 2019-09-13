287923789b3e6a7eff1346e05db2c60402f59efc
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-13 11:30:30 +0200

    copied

---
+++ week-01/day-5/draw-triangle.ts
@@ -0,0 +1,15 @@
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+let stars: string = "";
+for (let i:number = 0; i < lineCount; i++){
+    stars = stars + "*";
+    console.log(stars);
+}

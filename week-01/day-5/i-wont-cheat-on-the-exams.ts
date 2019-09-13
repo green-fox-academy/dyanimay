26f48faf2bc7b8f9bca156761ab04872336ebe3f
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-13 10:33:03 +0200

    it is long

---
+++ week-01/day-5/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,8 @@
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+let cheat: string = "I won´t cheat on the exam!";
+let exam: number = 0;
+while (exam < 100) {
+    console.log(cheat);
+    exam++;
+}

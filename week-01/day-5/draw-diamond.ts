f78463f2c489e0519520998fc2d61052b17f7a8b
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-13 13:51:24 +0200

    with Daniel Tiringer

--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -10,17 +10,18 @@ function renderDiamond(n) {
       }
       console.log(str);
     }
-function renderDiamond (n) {
-    for (let i = n; i > n; i--) {
-        let str = "*";
-        for (let j = 1; j > n+i; j--) {
-            str = str + " ";
+};
+function renderDiamond2(n) {
+    for (let i = 0; i < n-1; i++) {
+        let str = "";
+        for (let j = 1; j <= i+1; j++) {
+          str = str + " ";
         }
-        for (let k = 1; k <= (2*i+1); k--) {
-            str = str + "*";
+        for (let k = 1; k < (2*(n-i-1)); k++) {
+          str = str + "*";
         }
         console.log(str);
-    }
-  }
+      }
 }
-  renderDiamond(7)
+  renderDiamond(4);
+  renderDiamond2(4);

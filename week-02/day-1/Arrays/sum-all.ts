3f39f2235dde5e06c4dd570598bd05056e9f00e7
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-16 14:15:09 +0200

    how to reduce

---
+++ week-02/day-1/Arrays/sum-all.ts
@@ -0,0 +1,6 @@
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+let ai = [3, 4, 5, 6, 7];
+// -  Log the sum of the elements in `ai` to the console
+let sum = ai.reduce((a, b) => (a + b));
+
+console.log(sum);

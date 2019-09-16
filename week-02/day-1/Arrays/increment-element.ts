9004b2893ad7a96d22b08d12d25ff6545e7c8952
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-16 12:34:33 +0200

    simply accessed

---
+++ week-02/day-1/Arrays/increment-element.ts
@@ -0,0 +1,7 @@
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+let numList2 = [1, 2, 3, 4, 5];
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the console
+numList2[2] = numList2[2] + 1;
+console.log(numList2[2]);
+console.log(numList2);

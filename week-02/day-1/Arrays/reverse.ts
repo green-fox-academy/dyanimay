7ec1eb0a3238b3a75a96d4568ca6ea243a09a352
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-16 16:16:54 +0200

    2 solutions

--- week-02/day-1/Arrays/reverse.ts
+++ week-02/day-1/Arrays/reverse.ts
@@ -7,3 +7,12 @@
 let numListReverse: number[] = [3, 4, 5, 6, 7];
 numListReverse.reverse();
 console.log(numListReverse);
+
+//Second solution
+let numListReverse2: number[] = [3, 4, 5, 6, 7, 8];
+for(let i=0; i <= Math.floor((numListReverse2.length-1) / 2); i++){
+    let Temporary = numListReverse2[i];
+    numListReverse2[i] = numListReverse2[numListReverse2.length - 1 - i];
+    numListReverse2[numListReverse2.length - 1 - i] = Temporary;
+}
+console.log(numListReverse2);

d07857bfff6468180e8bbe750c4e77d08beb5fcd
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-16 11:22:01 +0200

    why :number isn't needed?

---
+++ week-02/day-1/Arrays/sum-elements.ts
@@ -0,0 +1,4 @@
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+let r = [51, 23, 66, 12];
+// -  Print the sum of the second and the third element
+console.log(r[1] + r[2]);

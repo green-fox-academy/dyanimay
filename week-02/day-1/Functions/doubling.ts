8dbed23c222c4a0c949c13a4823edce9d070f897
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-16 16:46:53 +0200

    not the training material style

---
+++ week-02/day-1/Functions/doubling.ts
@@ -0,0 +1,8 @@
+// -  Create a variable named `baseNum` and assign the value `123` to it
+let baseNum: number = doubling(123);
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+function doubling(a){
+    return a * 2;
+}
+// -  Log the result of `doubling(baseNum)` to the console
+console.log(baseNum);

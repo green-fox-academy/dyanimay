647178206c4144ee08664e56bd1ba962bde996ea
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-17 12:16:43 +0200

    google

---
+++ week-02/day-3/reverse.ts
@@ -0,0 +1,9 @@
+'use strict';
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+function reverse(reversed: string){
+    return reversed.split('').reverse().join('');
+}
+console.log(reverse(reversed));

b21b926b1f649c64f7199e4a6769442d4fc3292e
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-17 13:34:11 +0200

    simple way

---
+++ week-02/day-3/todo-print.ts
@@ -0,0 +1,17 @@
+'use strict';
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+
+// Expected outpt:
+
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+let todoText: string = ' - Buy milk\n';
+let myToDo: string = 'My todo:\n';
+let games: string = ' - Download games\n\t - Diablo';
+
+console.log(myToDo + todoText + games);

fad512007f60eabe3b77994bc17add08b61255ec
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-17 17:20:53 +0200

    change folder

--- week-02/day-2/data structure/List_introduction_1.ts
+++ week-02/day-2/data structure/List_introduction_1.ts
@@ -21,28 +21,37 @@ console.log(names[2]);
 //William
 //John
 //Amanda
-names.forEach(print);
-function print(names: string){
+names.forEach(printList);
+function printList(names: string){
     console.log(names)
 };
 /* OR
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+for (let list = 0; list < names.length; list++) {
+    console.log(names[list]);
 }
 */
 //Iterate through the list and print
 //1. William
 //2. John
 //3. Amanda
-names.forEach(value => console.log(names.indexOf(value) + 1 + '. ' + value));
+for (let list = 0; list < names.length; list++) {
+    console.log((list+1) + '. ' + names[list]);
+}
+//OR names.forEach(value => console.log(names.indexOf(value) + 1 + '. ' + value));
+
 //Remove the 2nd element
-names = names.filter(function(string){
-    return (string != 'John');
+names = names.filter(function(name){
+    return (name != 'John');
 })
+for (let whatever = 0; whatever < names.length; whatever++) {
+    console.log(names[whatever]);
+}
 //Iterate through the list in a reversed order and print out each name
 //Amanda
 //William
-console.log(names.reverse());
+for (let switch = 0; switch < names.length; switch++) {
+    console.log(names.reverse[switch];
+}
 //Remove all elements
 names.splice(0);
 console.log(names);

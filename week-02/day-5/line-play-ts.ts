5e763c8edc0eaf58be8134dbb7cf19b4ac4138eb
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-20 10:46:27 +0200

    there are 2 strange lines, but can not solve it

--- week-02/day-5/line-play-ts.ts
+++ week-02/day-5/line-play-ts.ts
@@ -3,25 +3,26 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
+const divide:number = 1;
 const num:number = 20;
 const w:number = canvas.width;
 const h:number = canvas.height;
 
-function lines(x1: number, x2: number, y1: number, y2:number){
+function lines(x1: number, y1: number, x2: number, y2:number){
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
 }
-for (let i:number = 1; i <= num; i = i + 1){
+for (let i:number = 1; i < num; i = i + 1){
     let x:number = i * w / (num - 1);
     let y:number = i * h / (num - 1);
     lines (0, y, x, h);
     ctx.strokeStyle = 'green';
 }
-for (let i:number = num; 0 <= i; i = i - 1){
+for (let i:number = 1; i < num; i = i + 1){
     let x:number = i * w / (num - 1);
     let y:number = i * h / (num - 1);
-    lines (x, w, 0, y);
+    lines (h, y, x, 0);
     ctx.strokeStyle = 'purple';
 }

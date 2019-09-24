bbfc95aa1cba14e0edc5dec5da5b12c5c18a2df5
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-09-24 19:07:34 +0200

    homework before reading the materials

---
+++ week-03/day-3/Homework/trees before materials.ts
@@ -0,0 +1,11 @@
+const type:string [] = ['Maples', 'Apple', 'Orange', 'Blackcherry', 'Apricot'];
+const leafColor:string [] = ['green', 'yellow', 'red', 'brown'];
+const age:number [] = [10, 20, 50, 100];
+const sex:string [] = ['male', 'female', 'asexual'];
+const tree: any [] = [type, leafColor, age, sex];
+
+let tree1 = ['Maples', 'green', 10, 'male'];
+let tree2 = ['Apple', 'yellow', 50, 'female'];
+let tree3 = ['Orange', 'green', 10, 'male'];
+let tree4 = ['Blackcherry', 'red', 20, 'asexual'];
+let tree5 = ['Apricot', 'brown', 100, 'female'];

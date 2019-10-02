64b41b476db12e2f3724c1ea077947a689d161f8
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-02 10:40:56 +0200

    apples was easy copy from material but I am proud that I could write class and create a methodgit status

---
+++ week-04/day-3/apples.ts
@@ -0,0 +1,16 @@
+export class Apple {
+  public getApple(): string{
+    return 'apple';
+  }
+}
+import * as test from 'tape';
+
+test('write apple', t => {
+  const apple1 = new Apple();
+
+  var actual = apple1.getApple();
+  var expected = 'apple';
+
+  t.equal(actual, expected);
+  t.end();
+})

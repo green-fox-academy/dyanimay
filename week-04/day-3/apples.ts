103f4ecb24bb16bbb9b6a18fa40f29389acf66ae
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-02 10:53:23 +0200

    creating a separate test file for all the cases

--- week-04/day-3/apples.ts
+++ week-04/day-3/apples.ts
@@ -3,14 +3,3 @@ export class Apple {
     return 'apple';
   }
 }
-import * as test from 'tape';
-
-test('write apple', t => {
-  const apple1 = new Apple();
-
-  var actual = apple1.getApple();
-  var expected = 'apple';
-
-  t.equal(actual, expected);
-  t.end();
-});
---
+++ week-04/day-3/tests.ts
@@ -0,0 +1,12 @@
+import {Apple} from './apples'
+import * as test from 'tape';
+
+test('write apple', t => {
+  const apple1 = new Apple();
+
+  var actual = apple1.getApple();
+  var expected = 'apple';
+
+  t.equal(actual, expected);
+  t.end();
+});

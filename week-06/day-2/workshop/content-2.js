f72c8d6af98dc41a12d5c7d9b401b53dd50a4e62
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-22 15:31:27 +0200

    added a useful comment to JS and created CSS for making background color

--- week-06/day-2/workshop/content-2.js
+++ week-06/day-2/workshop/content-2.js
@@ -6,5 +6,5 @@ let placeholderList = document.querySelectorAll('li');
 
 for (let j = 0; j < placeholderList.length; j++){
   placeholderList[j].textContent = list[j];
-  console.log('textContent:', list[j]);
+  console.log('textContent:', list[j]); //only list[j] is needed without .textContent, because it is already a content so we just need each element of the list "list"
 }

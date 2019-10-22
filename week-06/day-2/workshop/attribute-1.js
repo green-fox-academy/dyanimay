7765fbf3834f0193febb7be4ff3771133e4b3751
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-22 16:58:38 +0200

    picture is done for my favourite animal

---
+++ week-06/day-2/workshop/attribute-1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let link = document.querySelector('img');
+console.log(link.getAttribute('src'));
+link.setAttribute('src', 'https://hiros.hu/assets/cache/images/b3/b3d054ee40cef3abdc1ce69f6d292327.jpg');
+link.height = 100*2;
+link.width = 67.5*2;

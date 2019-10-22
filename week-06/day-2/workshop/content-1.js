067071def3ac59413a2eed5fe07bc0c9ea4d65e2
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-22 15:06:20 +0200

    content 1 is done with a bit of task explanation from Johny

--- week-06/day-2/workshop/content-1.js
+++ week-06/day-2/workshop/content-1.js
@@ -1,2 +1,12 @@
 'use strict'
 
+let lines = document.querySelectorAll('p');
+/*for (let i = 0; i < lines.length; i++){
+  lines[i].textContent = lines[lines.length-1].textContent;
+  console.log('textContent:', lines[lines.length-1].textContent);
+}*/
+
+for (let j = 0; j < lines.length; j++){
+  lines[j].innerHTML = lines[lines.length-1].innerHTML;
+  console.log('innerHTML:', lines[lines.length-1].innerHTML);
+}

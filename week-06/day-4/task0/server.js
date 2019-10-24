322b8368ccbfd9f13e2968b7e7362e9f8ecf01b7
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-24 13:51:48 +0200

    everything I did, it is a descriptive commit

---
+++ week-06/day-4/task0/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+app.use(express.static('assets'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// app.get('/about', function (req, res){
+//   res.send('This is me');
+// });
+
+app.listen(PORT);

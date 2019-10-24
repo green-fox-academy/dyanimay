4b416ec415ae85454adb98c5070bd0c148d24f7f
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-24 16:37:26 +0200

    todo list is done with forEach and with forloop too

--- week-06/day-4/Todos/index.js
+++ week-06/day-4/Todos/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// home page
-app.get('/', (req, res) => {
-  if (Object.entries(req.query).length === 0){ //it checks whether you wrote something or not, if not then it writes Guest
-    name = 'Guest';
-  } else {
-    name = req.query.name
+
+app.get('/', function(req, res){
+    res.render('home', {
+      todos
+    });
   }
-  res.render('home', {
-    title: `Welcome back ${name}!`, //I am using the trick here BUT you have to use the back comma `
-  });
-});
+)
 
 // start express app on port 3000
 app.listen(PORT, () => {

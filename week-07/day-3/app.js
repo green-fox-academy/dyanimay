1f36e2d3a05d60b73932404e2015427fc3883a67
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-30 11:35:40 +0100

    copying quickly what johny wrote

--- week-07/day-3/app.js
+++ week-07/day-3/app.js
@@ -34,3 +34,59 @@ app.listen(PORT, () => {
 // http://www.johnyka.org:8080/v1/audio-analysis/?include=beats
 // v1/audio-analysis/ -> Query Path
 // ?include=beats -> Query Parameters
+
+//REST API
+
+//[GET] /movies
+app.get('/movies', (req, res) => {
+  const movies = {
+    list: ['zombieland', 'godzilla', 'sharknado', 'zombeavers'];
+  }
+  
+  res.status(200);
+  res.setHeader('Content-type', 'application/json');
+  res.send(movies);
+});
+//[GET] /movies?genre=comedy
+app.get('/movies', (req, res) => {
+  console.log('query param: ' + req.query.genre);
+            
+  const movies = {
+    list: ['zombieland', 'zombeavers'];
+  };
+            
+  res.status(200);
+  res.setHeader('Content-type', 'application/json');
+  res.send(movies);
+  });
+//[GET] /movies/id
+app.get('/movies/:id', (req, res) => {
+  console.log('ID: ' + req.param.id);
+
+  var movies = {
+    title: 'zomieland',
+    genre: 'comedy',
+    release: 2010
+  }
+
+  res.status(200);
+  res.setHeader('Content-type', 'application/json');
+  res.send(movies);
+});
+
+//[POST] /movies
+app.post('/movies', (req, res) => {
+  console.log('The movie is successfully added.');
+  console.log(req.body);
+
+  var movies = {
+    title: 'zomieland',
+    genre: 'comedy',
+    release: 2010
+  }
+
+  res.status(200);
+  res.send('ok');
+});
+//[DELETE] /movies/id
+

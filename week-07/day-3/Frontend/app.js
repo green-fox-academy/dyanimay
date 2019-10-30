1c5090a41cac9194b3e58d8d8e4d425b87e08950
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-30 13:30:06 +0100

    doubling works in chrome, and else res status should be 200 not 400, because it is not a real error

--- week-07/day-3/Frontend/app.js
+++ week-07/day-3/Frontend/app.js
@@ -10,7 +10,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
+app.get('/doubling', (req, res) => {
+  let doubling = {};
+  //console.log(req.query.input);
+  res.setHeader("Content-type", "application/JSON");
+  if (req.query.input !== undefined) {
+      res.status(200);
+      doubling['received'] = req.query.input;
+      doubling['result'] = req.query.input * 2;
+  } else {
+      res.status(200); 
+      doubling = {
+          "error": "Please provide an input!"
+      }
+  }
+  res.send(doubling);
+})
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);

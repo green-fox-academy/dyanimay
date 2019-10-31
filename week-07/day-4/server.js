36a329e27e2ede17f9c39e8eeb29a65172eb1b8c
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-10-31 17:28:53 +0100

    old changes in day 4

--- week-07/day-4/server.js
+++ week-07/day-4/server.js
@@ -1,20 +1,34 @@
-const express = require ('express');
-const mysql = require ('mysql');
+const express = require('express');
+const mysql = require('mysql');
 const app = express();
+const table = 'rapgod';
+const PORT = 3000;
+const env = require('dotenv').config();
+
+app.use(express.json());
 
 let conn = mysql.createConnection ({
-  host: 'localhost',
-  user: 'jaj',
-  password: 'alma',
-  database: 'myDBname',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME 
 });
 
-conn.connect(function(err) {
-  if (err) {
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
 });
 
-conn.end();
+app.get('/', function(req, res) {
+  conn.query('SELECT * FROM rapgod;', function(err, rows) {
+    console.log(rows);
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.listen(PORT);

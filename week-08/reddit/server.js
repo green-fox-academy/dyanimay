54dd3e65b309bb22eddd711e2d61d83ecf1af6a5
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-04 09:56:41 +0100

    npms are installed and files are copied

---
+++ week-08/reddit/server.js
@@ -0,0 +1,176 @@
+const express = require('express');
+const mysql = require('mysql');
+const env = require('dotenv').config();
+var bodyParser = require('body-parser')
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+var jsonParser = bodyParser.json()
+
+let conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'user', //ezt írtam át
+  password: 'password',
+  database: 'rapgods'
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+let output = {}
+let currentUser = 'KRISTOFOS'
+
+app.get('/posts', function (req, res) {
+  conn.query('SELECT * FROM posts;', function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Accept", "application/json");
+    res.setHeader("Username", "username");        // should be changable username
+    res.status(200);
+    output = { "posts": rows }
+    res.send(output);
+  });
+});
+
+
+app.post('/posts', jsonParser, (req, res) => {
+  let response = req.body;
+  let sqlQuery = ("INSERT INTO posts VALUES(null,'" + req.body.title + "','" + req.body.url + "','" + Date.now() + "',1,'" + currentUser + "',1);");
+  conn.query(sqlQuery, function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Accept", "application/json");
+    res.setHeader("Username", "username");        // should be changable username
+    res.status(200);
+    conn.query('SELECT * FROM posts WHERE id = ?;', [rows.insertId], function (err, newRow) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send('Database error');
+        res.send();
+        return;
+      }
+      res.setHeader("Content-type", "application/json");
+      res.setHeader("Accept", "application/json");
+      res.setHeader("Username", "username");        // should be changable username
+      res.status(200);
+      // output = { "posts": rows }
+      output = { "posts": newRow }
+      res.send(output);
+    });
+
+  });
+
+
+})
+
+app.put('/posts/:id/upvote', (req, res) => {
+  let sqlQuery = ("UPDATE posts SET score = score + 1 WHERE id = "+ req.params.id+";");
+  conn.query(sqlQuery, function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Accept", "application/json");
+    res.setHeader("Username", "username");        // should be changable username
+    res.status(200);
+    conn.query('SELECT * FROM posts WHERE id = ?;', [req.params.id], function (err, newRow) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send('Database error');
+        res.send();
+        return;
+      }
+      res.setHeader("Content-type", "application/json");
+      res.setHeader("Accept", "application/json");
+      res.setHeader("Username", "username");        // should be changable username
+      res.status(200);
+      // output = { "posts": rows }
+      output = { "posts": newRow }
+      res.send(output);
+    });
+
+  });
+
+})
+
+app.put('/posts/:id/downvote', (req, res) => {
+  let sqlQuery = ("UPDATE posts SET score = score - 1 WHERE id = "+ req.params.id+";");
+  conn.query(sqlQuery, function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Accept", "application/json");
+    res.setHeader("Username", "username");        // should be changable username
+    res.status(200);
+    conn.query('SELECT * FROM posts WHERE id = ?;', [req.params.id], function (err, newRow) {
+      if (err) {
+        console.log(err.toString());
+        res.status(500).send('Database error');
+        res.send();
+        return;
+      }
+      res.setHeader("Content-type", "application/json");
+      res.setHeader("Accept", "application/json");
+      res.setHeader("Username", "username");        // should be changable username
+      res.status(200);
+      // output = { "posts": rows }
+      output = { "posts": newRow }
+      res.send(output);
+    });
+
+  });
+})
+
+app.delete('/posts/:id', (req, res) => {
+  conn.query('SELECT * FROM posts WHERE id = '+ req.params.id +';', function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+    output = rows
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Accept", "application/json");
+    res.setHeader("Username", "username");        // should be changable username
+    res.status(200);
+    res.send(output);
+  });
+  let sqlQuery = ("DELETE FROM posts WHERE id = "+ req.params.id+" ;");
+  conn.query(sqlQuery, function (err, rows) {
+    if (err) {
+      console.log(err.toString());
+      res.status(500).send('Database error');
+      res.send();
+      return;
+    }
+  });
+
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});

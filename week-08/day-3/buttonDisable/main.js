491075fccc3080b20d3a9256a0d9064411efaf7d
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-06 16:43:27 +0100

    button disable is finished

--- week-08/day-3/buttonDisable/main.js
+++ week-08/day-3/buttonDisable/main.js
@@ -9,17 +9,27 @@ function click(){
     btn[1].removeAttribute('disabled');
   } else if (fact[0].checked == true){
     btn[0].removeAttribute('disabled');
+  } else {
+    btn[1].removeAttribute('disabled');
   };
 }
 
 function loveCats(){
   if (pet[0].checked == true || pet[1].checked == true || fact[0].checked == true){
-  alert("Thank you, you've successfully signed up for cat facts");
+    alert("Thank you, you've successfully signed up for cat facts");
   };
-};
+}
 
+function hateCats(){
+  if (pet[2].checked == true && fact[1].checked == true){
+    alert('Sigh, we still added you to the cat facts list');
+  }
+}
+
+//pet listeners
 pet[0].addEventListener('click', function(){
   pet[0].checked == true;
+  pet[2].checked == false;
   click();
 });
 
@@ -28,9 +38,21 @@ pet[1].addEventListener('click', function(){
   click();
 });
 
+pet[2].addEventListener('click', function(){
+  pet[0].checked == false;
+  pet[1].checked == false;
+  pet[2].checked == true;
+});
+//fact listeners
 fact[0].addEventListener('click', function(){
   fact[0].checked == true;
-  click();
+  fact[1].checked == false;
+  btn[0].removeAttribute('disabled');
+})
+fact[1].addEventListener('click', function(){
+  fact[1].checked == true;
+  fact[0].checked == false;
+  btn[0].disabled = true;
 })
 // clicking on the 2 buttons 
 btn[0].addEventListener('click', function(){
@@ -40,5 +62,9 @@ btn[0].addEventListener('click', function(){
 
 btn[1].addEventListener('click', function(){
   event.preventDefault();
-  loveCats();
+  if (pet[2].checked == true && fact[1].checked == true){
+    hateCats();
+  } else {
+    loveCats();
+  }
 });

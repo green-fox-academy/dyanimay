aa762ec7a2fd703987553d1ebb80119042111780
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-07 13:26:32 +0100

    refreshed the matReview files what Viktor pushed to git

--- week-08/day-4/matReview/server.js
+++ week-08/day-4/matReview/server.js
@@ -4,12 +4,22 @@ const PORT = 8080;
 
 let app = express();
 app.use(express.json());
+app.set('port', (process.env.PORT || PORT));
 
-app.get('/batman', (req, res) => {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.send('Batman changed the calendar');
+app.get('/batman', (request, response) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.send('Batman changed the calendar');
 });
 
-app.listen(PORT, () => {
+app.post('/batman', (request, response) => {
+    setTimeout(function () {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.status(404);
+        response.send();
+        //response.send('Batman changed the calendar via POST');
+    }, 2000);
+});
 
+app.listen(app.get('port'), () => {
+    console.log(`\nServer is running\nListening on port ${PORT}`);
 });

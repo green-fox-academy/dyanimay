eaa2cb77f9d5a85eda195a1786fae37c0f6a8c73
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-12 14:24:02 +0100

    first test is done with comments and with dynamic query message

--- week-09/day-2/iamgroot/test/test-routes.js
+++ week-09/day-2/iamgroot/test/test-routes.js
@@ -3,7 +3,20 @@
 const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
-​
+
 test('groot endpoint', (t) => {
-  // TODO: implement it
+  request(app)
+    .get('/groot') //it makes a request to these end points
+    .query({message: 'lol'}) //ezzel érem el hogy dinamikus legyen a teszt, itt adom meg hogy a receivednél milyen messaget írtam volna be
+    .expect('Content-Type', /json/) //the header can be anything but has to be in json
+    .expect(200)
+      .end(function (err, res) {
+        let message = {
+          "received": "lol",
+          "translated": "I am Groot!"
+      };
+        t.error(err, 'No error'); //ez az első teszt
+        t.deepEqual(res.body, message, 'Correct message'); //ez a második teszt, hogy amit visszakaptunk (res.body) az egyezik-e az általam megadott messsage változóval
+        t.end();
+      });
 });

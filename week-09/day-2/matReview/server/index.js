4f897eb99b93546b4ccebadaaf96cfe25b79de5a
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-12 13:18:55 +0100

    changed the matReview because I was left behind with typing it

--- week-09/day-2/matReview/server/index.js
+++ week-09/day-2/matReview/server/index.js
@@ -3,56 +3,91 @@
 let express = require('express');
 let app = express();
 
+const bodyParser = require("body-parser");
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
 let pokemonDatabase = [
-  {
-    id: 1,
-    name: 'Pikachu',
-    type: 'electric',
-    level: 24
-  },
-  {
-    id: 2,
-    name: 'Charmander',
-    type: 'fire',
-    level: 24
-  },
-  {
-    id: 3,
-    name: 'Togepi',
-    type: 'normal',
-    level: 24
-  },
-  {
-    id: 4,
-    name: 'Ratata',
-    type: 'normal',
-    level: 99
-  }
+    {
+        id: 1,
+        name: "Pikachu",
+        type: "electric",
+        level: 10
+    },
+    {
+        id: 2,
+        name: "Charmander",
+        type: "fire",
+        level: 54
+    },
+    {
+        id: 3,
+        name: "Togepi",
+        type: "normal",
+        level: 24
+    },
+    {
+        id: 4,
+        name: "Ratata",
+        type: "normal",
+        level: 99
+    }
 ]
 
 app.get('/api/pokemon', function (req, res) {
-  res.status(200);
-  res.json(pokemonDatabase);
+    let type = req.query.type;
+    let filteredPokemons = pokemonDatabase;
+
+    if(type){
+        filteredPokemons = pokemonDatabase.filter(obj =>{
+            return obj.type == type;
+        });
+    }
+
+    res.status(200);
+    res.json(filteredPokemons); 
 });
 
 app.get('/api/pokemon/:id', function (req, res) {
-  let id = req.params.id;
-  let pokemon = pokemonDatabase.find(obj => {
-    return obj.id == id;
-  });
+    let id = req.params.id;
+    let pokemon = pokemonDatabase.find(obj => {
+        return obj.id == id
+    });
 
-  res.status(200);
-  res.json(pokemon);
+    if(pokemon){
+        res.status(200);
+        res.json(pokemon);
+    }else{
+        res.status(404);
+        res.json({"error": "No pokemon found with this ID"});
+    }
 });
 
 app.post('/api/pokemon', function (req, res) {
-  let body = req.body
-
-  if(body.type && body.name && body.level && Object.keys(body).length == 3){
+    let body = req.body;
     
-  };
+    if(body.type && body.name && body.level && Object.keys(body).length == 3){
+        let maxID = Math.max.apply(Math, pokemonDatabase.map(function(o) { return o.id; }));
+        body.id = maxID + 1;
 
-  pokemonDatabase.push(body);
+        pokemonDatabase.push(body);
+        res.status(200);
+        res.json({"id":  maxID + 1});
+    }else{
+        res.status(400);
+        res.json({"error": "Not a valid pokemon object"});
+    }
 });
 
 module.exports = app; //this helps to use the app
--- week-09/day-2/matReview/test/index.js
+++ week-09/day-2/matReview/test/index.js
@@ -26,10 +26,149 @@ test('Correct Pokemons returned', function(t) {
     .get('/api/pokemon/3') //it makes a request to these end points
     .expect('Content-Type', /json/) //the header can be anything but has to be in json
     .expect(200)
-    .end(function(err, res) {
-      t.error(err, 'No error');
-      t.equal(res.body.name, 'Togepi', 'Correct name');
-      t.equal(res.body, 'Pikachu', 'Correct name of the first pokemon')
-      t.end();
-    });
-});
+      .end(function (err, res) {
+
+        let pokemon = {"id":3,"name":"Togepi","type":"normal","level":24}
+        t.error(err, 'No error');
+        t.deepEqual(res.body, pokemon, 'Correct pokemon');
+        t.end();
+      });
+  });
+
+  test('Correct pokemons returned by ID', function (t) {
+    request(app)
+      .get('/api/pokemon/5')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.deepEqual(res.body, {"error": "No pokemon found with this ID"}, 'Correct pokemon');
+        t.end();
+      });
+  });
+
+  test('Correct pokemons returned by type normal', function (t) {
+    request(app)
+      .get('/api/pokemon')
+      .query({ type: 'normal'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        let pokemons = [
+            {
+                id: 3,
+                name: "Togepi",
+                type: "normal",
+                level: 24
+            },
+            {
+                id: 4,
+                name: "Ratata",
+                type: "normal",
+                level: 99
+            }
+        ]
+        t.error(err, 'No error');
+        t.deepEqual(res.body, pokemons, 'Correct normal pokemons');
+        t.end();
+      });
+  });
+
+  test('Correct pokemons returned by ID', function (t) {
+    request(app)
+      .get('/api/pokemon/5')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.deepEqual(res.body, {"error": "No pokemon found with this ID"}, 'Correct pokemon');
+        t.end();
+      });
+  });
+
+  test('Correct pokemons returned by type water', function (t) {
+    request(app)
+      .get('/api/pokemon')
+      .query({ type: 'water'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.deepEqual(res.body, [], 'Correct water pokemons');
+        t.end();
+      });
+  });
+
+  test('Correct pokemons returned by type water', function (t) {
+    request(app)
+      .get('/api/pokemon')
+      .query({ typee: 'normal', owner: 'ash'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+
+        let pokemons = [
+            {
+                id: 1,
+                name: "Pikachu",
+                type: "electric",
+                level: 10
+            },
+            {
+                id: 2,
+                name: "Charmander",
+                type: "fire",
+                level: 54
+            },
+            {
+                id: 3,
+                name: "Togepi",
+                type: "normal",
+                level: 24
+            },
+            {
+                id: 4,
+                name: "Ratata",
+                type: "normal",
+                level: 99
+            }
+        ]
+
+        t.error(err, 'No error');
+        t.deepEqual(res.body, pokemons, 'Correct water pokemons');
+        t.end();
+      });
+  });
+
+  test('Correct new pokemon', function (t) {
+    request(app)
+      .post('/api/pokemon')
+      .send({
+        "name": "Slowpoke",
+        "type": "water",
+        "level": 2
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.deepEqual(res.body, {"id": 5}, 'Correct id');
+        t.end();
+      });
+  });
+
+  test('Correct new pokemon', function (t) {
+    request(app)
+      .post('/api/pokemon')
+      .send({
+        "name": "Slowpoke",
+        "type": "water",
+      })
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.deepEqual(res.body, {"error": "Not a valid pokemon object"}, 'Failed successfully');
+        t.end();
+      });
+  });

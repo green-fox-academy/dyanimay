8a63324b3cf44e09c27b5f75d82e2baac5f44c88
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-12 13:15:24 +0100

    I am groot is copied from the task and mat review is rewritten

--- week-09/day-2/matReview/server/index.js
+++ week-09/day-2/matReview/server/index.js
@@ -2,10 +2,57 @@
 
 let express = require('express');
 let app = express();
-let users = ['John', 'Betty', 'Hal'];
 
-app.get('/api/users', function (req, res) {
-  res.json(users);
+let pokemonDatabase = [
+  {
+    id: 1,
+    name: 'Pikachu',
+    type: 'electric',
+    level: 24
+  },
+  {
+    id: 2,
+    name: 'Charmander',
+    type: 'fire',
+    level: 24
+  },
+  {
+    id: 3,
+    name: 'Togepi',
+    type: 'normal',
+    level: 24
+  },
+  {
+    id: 4,
+    name: 'Ratata',
+    type: 'normal',
+    level: 99
+  }
+]
+
+app.get('/api/pokemon', function (req, res) {
+  res.status(200);
+  res.json(pokemonDatabase);
+});
+
+app.get('/api/pokemon/:id', function (req, res) {
+  let id = req.params.id;
+  let pokemon = pokemonDatabase.find(obj => {
+    return obj.id == id;
+  });
+
+  res.status(200);
+  res.json(pokemon);
+});
+
+app.post('/api/pokemon', function (req, res) {
+  let body = req.body
+
+  if(body.type && body.name && body.level && Object.keys(body).length == 3){
+    
+  };
+
+  pokemonDatabase.push(body);
 });
 
 module.exports = app; //this helps to use the app
--- week-09/day-2/matReview/test/index.js
+++ week-09/day-2/matReview/test/index.js
@@ -1,23 +1,35 @@
-"use strict";
+'use strict';
 
-var test = require("tape");
-var request = require("supertest");
-var app = require("../server");
+var test = require('tape');
+var request = require('supertest');
+var app = require('../server');
 
-test("First test!", function(t) {
+test('First test!', function(t) {
   t.end();
 });
 
-test("Correct users returned", function(t) {
+test('Correct Pokemons returned', function(t) {
   request(app)
-    .get("/api/users") //it makes a request to these end points
-    .expect("Content-Type", /json/) //the header can be anything but has to be in json
+    .get('/api/pokemon') //it makes a request to these end points
+    .expect('Content-Type', /json/) //the header can be anything but has to be in json
     .expect(200)
     .end(function(err, res) {
-      var expectedUsers = ["John", "Betty", "Hal"];
+      t.error(err, 'No error');
+      t.equal(res.body.length, 4, 'Correct number of pokemons');
+      t.equal(res.body[0].name, 'Pikachu', 'Correct name of the first pokemon')
+      t.end();
+    });
+});
 
-      t.error(err, "No error");
-      t.same(res.body, expectedUsers, "Users as expected");
+test('Correct Pokemons returned', function(t) {
+  request(app)
+    .get('/api/pokemon/3') //it makes a request to these end points
+    .expect('Content-Type', /json/) //the header can be anything but has to be in json
+    .expect(200)
+    .end(function(err, res) {
+      t.error(err, 'No error');
+      t.equal(res.body.name, 'Togepi', 'Correct name');
+      t.equal(res.body, 'Pikachu', 'Correct name of the first pokemon')
       t.end();
     });
 });

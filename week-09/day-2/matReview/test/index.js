cf66f92eb7cfa63e678ac84f116802cd984c41b8
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-12 10:28:14 +0100

    mat review is done and it works

--- week-09/day-2/matReview/test/index.js
+++ week-09/day-2/matReview/test/index.js
@@ -1,7 +1,23 @@
-'use strict';
+"use strict";
 
-var test = require('tape');
+var test = require("tape");
+var request = require("supertest");
+var app = require("../server");
 
-test('First test!', function (t) {
+test("First test!", function(t) {
   t.end();
+});
+
+test("Correct users returned", function(t) {
+  request(app)
+    .get("/api/users") //it makes a request to these end points
+    .expect("Content-Type", /json/) //the header can be anything but has to be in json
+    .expect(200)
+    .end(function(err, res) {
+      var expectedUsers = ["John", "Betty", "Hal"];
+
+      t.error(err, "No error");
+      t.same(res.body, expectedUsers, "Users as expected");
+      t.end();
+    });
 });

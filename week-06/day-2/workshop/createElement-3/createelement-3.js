6df99b646ca7632bf1ab10f82f427bc18265fbb7
Daniel Kristaly <kristaly.daniel92@gmail.com>
2019-11-19 16:13:09 +0100

    create element no3. is done with forEach and my own style: text content change

---
+++ week-06/day-2/workshop/createElement-3/createelement-3.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+let kidArticle = document.querySelector('#pets'); //mert ID=pets
+
+kids.forEach(kid => {
+  let article = document.createElement('article');
+  let h3 = document.createElement('h3');
+  let paragraph = document.createElement('p');
+
+  h3.textContent = kid.owner;
+  paragraph.textContent = kid.petName;
+
+  article.appendChild(h3);
+  article.appendChild(paragraph);
+  kidArticle.appendChild(article);
+});

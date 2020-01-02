f5d6bad3e41bc400b249c0772a640dae53efb5c8
Daniel Kristaly <kristaly.daniel92@gmail.com>
2020-01-02 14:10:21 +0100

    finished the hero app tutorial

--- week-11/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ week-11/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -8,7 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '/detail/:id', component: HeroDetailComponent},
+  { path: 'detail/:id', component: HeroDetailComponent},
 ];
 
 @NgModule({
